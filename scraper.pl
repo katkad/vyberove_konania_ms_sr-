8756634ebdd8b6c2c2e1c873810c0d020a5ef26c
Katarina Durechova <katarina.durechova@nic.cz>
2014-11-04 12:05:24 +0100

    Test if we can save a record in db

--- scraper.pl
+++ scraper.pl
@@ -61,10 +61,10 @@ sub do_detail
 		$v = new URI ($link->[0])->abs ($resp->request->uri)->as_string
 			if $link;
 
-		$row{$k} = $v;
+		$row{'aa'} = 'bb';
 	}
 
-	print $row{"D\x{e1}tum uz\x{e1}vierky"} . "\n";
+#	print $row{"D\x{e1}tum uz\x{e1}vierky"} . "\n";
 
 	$dt->upsert (\%row);
 }
